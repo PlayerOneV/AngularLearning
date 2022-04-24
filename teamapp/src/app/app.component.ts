4430c3c46091f03e4f0c699c6f47adc5db36a035
PlayerOne <player_one_117@outlook.com>
2022-04-24 02:03:45 -0500

    componente equipo, paso de valores entre componentes y logica para crear equipos

--- teamapp/src/app/app.component.ts
+++ teamapp/src/app/app.component.ts
@@ -6,8 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  newMemberName = ""
   members: string[] = [];
+  newMemberName = ""
+  teamsNumber: number | "" = ""
+  teams: string[][] = []
   errorMessage = ""
 
   getnameMember(member: string) {
@@ -25,8 +27,47 @@ export class AppComponent {
       this.members.push(this.newMemberName)
       this.newMemberName = "" //Limpiamos el input
       this.errorMessage = ""
+      this.teams = []
     }
 
     //console.log(this.members)
   }
+
+  getTeamsNumber(number: String) {
+    this.teamsNumber = Number(number)
+  }
+
+  generateTeams() {
+
+    if (!this.teamsNumber || this.teamsNumber <= 0) {
+      this.errorMessage = "Invalid number of teams"
+      return
+
+    } else if (this.members.length < this.teamsNumber) {
+      this.errorMessage = "Not enogth members"
+    } else {
+      this.errorMessage = ""
+      const allMembers = [...this.members]
+
+      while (allMembers.length) {
+        for (let i = 0; i < this.teamsNumber; i++) {
+
+          const element = Math.floor(Math.random() * allMembers.length) //Asignamos una posición al azar que tomaremos
+          const member = allMembers.splice(element, 1)[0] //Tomamos el valor en dicha posición guardandolo en un nuevo array y extrayendolo de allMembers
+
+          if (!member) break //Si ya no hay mas miembros por agregar 
+
+          if (this.teams[i]) { //Si ya esta ocupada la posición en el array agregamos el nuevo miembro al final
+            this.teams[i].push(member)
+          } else { //Si no esta ocupada la posición del array asignamos al nuevo miembro 
+            this.teams[i] = [member]
+          }
+
+        }
+      }
+      this.members = []
+      this.teamsNumber = ""
+    }
+  }
 }
+
