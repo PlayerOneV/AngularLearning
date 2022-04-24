306fb63c53f2313ec211adb177cc65a5fb5b6874
PlayerOne <player_one_117@outlook.com>
2022-04-23 20:03:12 -0500

    Primera parte logica completa. eventos, funciones, atributos

--- teamapp/src/app/app.component.ts
+++ teamapp/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName = ""
+  members: string[] = [];
+  errorMessage = ""
+
+  getnameMember(member: string) {
+    this.newMemberName = member
+
+    //console.log(this.newMemberName)
+  }
+
+  addMember() {
+
+    if (!this.newMemberName) {//Si no hay nada en el input para guardar
+      this.errorMessage = "Name can't be empty"
+      return
+    } else {
+      this.members.push(this.newMemberName)
+      this.newMemberName = "" //Limpiamos el input
+      this.errorMessage = ""
+    }
+
+    //console.log(this.members)
+  }
 }
