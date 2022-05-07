03a331c6a25fba226d5be367bc59600778ce1858
PlayerOne <player_one_117@outlook.com>
2022-05-07 16:07:29 -0500

    Calculadora con operaciones básicas completa usando bootstrap, event biding y two way binding

--- angularBasics/src/app/app.component.ts
+++ angularBasics/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularBasics';
+  title = 'Calculadora básica con Bootstrap';
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+
+  suma(): void {
+    this.result = this.num1 + this.num2;
+  }
+
+  resta(): void {
+    this.result = this.num1 - this.num2;
+  }
+
+  mult(): void {
+    this.result = this.num1 * this.num2;
+  }
+
+  div(): void {
+    this.result = this.num1 / this.num2;
+  }
 }
